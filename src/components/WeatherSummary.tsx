452decd42c91a3f9deac989a007e06b51b9c313a
Daithi333 <daithi@hotmail.co.uk>
2021-05-27 19:48:18 +0100

    added forcast

--- src/components/WeatherSummary.tsx
+++ src/components/WeatherSummary.tsx
@@ -2,8 +2,9 @@ import { FC } from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import {WeatherLocation, Weather} from "../model/Weather";
-import { readWeather } from "../services/WeatherService";
+import { readForecast, readWeather } from "../services/WeatherService";
 import { WeatherEntry } from "./WeatherEntry";
+import './WeatherSummary.scss';
 
 interface WeatherSummaryProps {
     location: WeatherLocation | null;
@@ -11,20 +12,39 @@ interface WeatherSummaryProps {
   
   export const WeatherSummary: FC<WeatherSummaryProps> = ({location}) => {
     const [weather, setWeather] = useState<Weather | null>(null);
+    const [forecast, setForecast] = useState<Weather[] | null>(null);
   
     useEffect(() => {
-      if (location) {
-        readWeather(location.id).then(weather => setWeather(weather));
-      }
+      (async function () {
+        if (location) {
+          const [weather, forecast] = await Promise.all([
+            readWeather(location.id),
+            readForecast(location.id)
+          ]);
+          setWeather(weather);
+          setForecast(forecast);
+        }
+      })();
     }, [location]);
 
-    if (!location || !weather) return null;
+    if (!location || !weather || !forecast) return null;
 
     return (
         <div>
           <hr/>
           <h2>{location.name}</h2>
           <WeatherEntry weather={weather}/>
+
+          <h2>Forecast</h2>
+          <div>
+            <ol>
+              {forecast.map(timePoint =>
+                <li key={timePoint.dt}>
+                  <WeatherEntry weather={timePoint}/>
+                </li>
+              )}
+            </ol>
+          </div>
         </div>
       );
 };
